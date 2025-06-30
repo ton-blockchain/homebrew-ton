9c917cdb349cf2d6e112effb9645c0b3c1a7dfec
runner <runner@Mac-1751270981889.local>
2025-06-30 08:24:29 +0000

    create release

--- ton.rb
+++ ton.rb
@@ -4,10 +4,10 @@ class Ton < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/ton-blockchain/homebrew-ton/releases/latest/download/ton-brew-arm64.tar.gz"
-    sha256 "dee79b6754fae2a771f53a441c9bcbdd3db7b6547042f719c01828586d3fed5c" #arm64
+    sha256 "385d15097eca477e9b81c50e3c8f877d204340148fb1099ad74a09e40bbfdbaf" #arm64
   else
     url "https://github.com/ton-blockchain/homebrew-ton/releases/latest/download/ton-brew-x86-64.tar.gz"
-    sha256 "b6d907b05454df160bf17bae8b07d3e5565ac257352d27a96da9da3311a33067" #amd64
+    sha256 "ef0c606c16785f15c05ac0c75b43e5f040a35b0fae144de62f995af3a597a203" #amd64
   end
 
   license "LGPL-2.0-only"
